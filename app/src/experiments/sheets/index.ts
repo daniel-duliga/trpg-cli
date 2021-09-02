ee870e4201803a1397d54bdeb094cafc99d13cec
Daniel Duliga <daniel.vasile.duliga@gmail.com>
2021-09-02 19:32:52 +0300

    Better format for sheets

--- app/src/experiments/sheets/index.ts
+++ app/src/experiments/sheets/index.ts
@@ -3,17 +3,12 @@ import fs from 'fs'
 
 import { rollDiceFormula } from '../../utils/dice-util'
 
-const rollCommand = '$roll_dice'
-
 const sheet = fs.readFileSync('app/src/experiments/sheets/sheet.yml', 'utf-8')
 const parsedSheet = YAML.parse(sheet)
-for (const prop in parsedSheet) {
-    const propValue = parsedSheet[prop]
-    if (propValue.startsWith(rollCommand)) {
-        let rollFormula = propValue.replace(rollCommand, '')
-        rollFormula = rollFormula.slice(1, rollFormula.length - 1)
-        const roll = rollDiceFormula(rollFormula)
-        parsedSheet[prop] = roll.value
+for (const fieldName in parsedSheet) {
+    const field = parsedSheet[fieldName]
+    if (field.type === 'dice_roll') {
+        field.value = rollDiceFormula(field.value).value
         continue
     }
 }
