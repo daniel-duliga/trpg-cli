a020251eafe49440d4dfa1a04c83f63608ba3585
Daniel Duliga <daniel.vasile.duliga@gmail.com>
2021-09-19 00:11:36 +0300

    Add support for 'section' fields in sheets
    Also ignored  `data/entities`.

--- app/src/utils/sheets-util.ts
+++ app/src/utils/sheets-util.ts
@@ -1,10 +1,10 @@
 import fs from 'fs'
 import YAML from 'yaml'
 
-import { dataBasePaths, fileExtensions } from '../constants';
-import { DiceUtil } from './dice-util';
-import { FileUtil } from "./file-util";
-import { TableUtil } from './tables-util';
+import { dataBasePaths, fileExtensions } from '../constants'
+import { DiceUtil } from './dice-util'
+import { FileUtil } from "./file-util"
+import { TableUtil } from './tables-util'
 
 export class SheetsUtil {
     static getAllSheets(): string[] {
@@ -14,6 +14,10 @@ export class SheetsUtil {
     static rollSheet(sheetPath: string): any {
         const sheetFile = fs.readFileSync(`${dataBasePaths.sheets}/${sheetPath}${fileExtensions.yml}`, 'utf-8')
         const sheet = YAML.parse(sheetFile)
+        return SheetsUtil.processSheet(sheet)
+    }
+
+    private static processSheet(sheet: any): any {
         for (const fieldName in sheet) {
             const field = sheet[fieldName]
             switch (field.type) {
@@ -28,6 +32,10 @@ export class SheetsUtil {
                 case 'eval': {
                     const formula = field.value.replace(/\$/g, 'sheet.')
                     sheet[fieldName] = eval(formula)
+                    break
+                }
+                case 'section': {
+                    sheet[fieldName] = SheetsUtil.processSheet(field.value)
                 }
             }
         }
